e8f5f4a25d5d1926bf21167c50794e17cbac5103
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-07 22:36:23 -0300

    feat: Praticando Cookies e Headers em FastAPI

--- apis_assincronas_fastapi/dio-blog/main.py
+++ apis_assincronas_fastapi/dio-blog/main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from typing import Annotated
 
-from fastapi import FastAPI, status
+from fastapi import Response, Cookie, FastAPI, status, Header
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,7 +14,18 @@ fake_db = [
 ]
 
 @app.get("/posts")
-def read_posts(published: bool, limit: int, skip: int = 0):
+def read_posts(
+    response: Response,
+    published: bool, 
+    limit: int, 
+    skip: int = 0, 
+    ads_id: Annotated[str | None, Cookie()] = None,
+    user_agent: Annotated[str | None, Header()] = None
+):
+
+    response.set_cookie(key="user", value="gabrieeltech@gmail.com")
+    print(f"Cookie: {ads_id}")
+    print(f"User-Agent: {user_agent}")
     return { "posts": [post for post in fake_db[skip: skip + limit] if post["published"] is published] 
             
         # posts = []
