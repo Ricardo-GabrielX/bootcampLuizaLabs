443ff14a3afe7b407437a955d24595b38bf6659d
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-07 21:31:35 -0300

    feat: utilizando query parameters, entendendo como funciona na prática, tipagem, ordem e prática

--- apis_assincronas_fastapi/dio-blog/main.py
+++ apis_assincronas_fastapi/dio-blog/main.py
@@ -4,6 +4,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_db = [
+    {"title": "Criando uma aplicação com Django", "date": datetime.now(), "published": True},
+    {"title": "Intercionalizando uma app FastAPI", "date": datetime.now(), "published": True},
+    {"title": "Criando uma aplicação com Flask", "date": datetime.now(), "published": False},
+    {"title": "Criando uma aplicação com Starlette", "date": datetime.now(), "published": True}
+]
+
+@app.get("/posts")
+def read_posts(published: bool, skip: int = 0, limit: int = len(fake_db)):
+    return { "posts": [post for post in fake_db[skip: skip + limit] if post["published"] is published] }
+# Argumentos obrigátorios devem vir antes dos opcionais, caso contrário, o FastAPI não conseguirá identificar os parâmetros corretamente e retornará um erro.
+# posts?published=on -> posts?published=true
+# posts?published=off -> posts?published=false
+
+
 @app.get("/posts/{framework}")
 def read_posts(framework: str):
     return {
--- apis_assincronas_fastapi/dio-blog/notes.md
+++ apis_assincronas_fastapi/dio-blog/notes.md
@@ -3,3 +3,5 @@
 import fastapi
 print(fastapi.__version__)
 
+> uvicorn <nomeArquivo>:<name> --reload
+> Example: --> uvicorn main:app --reload
