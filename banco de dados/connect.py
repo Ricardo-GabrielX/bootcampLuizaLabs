d5dd49df88fa11f39ecff362561a80551a36b03a
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-14 14:25:38 -0300

    feat: inserindo registros e colocando cada instrução dentro de uma função

--- banco de dados/connect.py
+++ banco de dados/connect.py
@@ -8,9 +8,22 @@ conexao = sqlite3.connect(ROOT_PATH / 'meu_banco.sqlite')
 print(conexao)
 cursor = conexao.cursor()
 
-# cursor.execute('CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(80), email VARCHAR(150))')
+
+def criar_tabela(cursor, conexao):
+    cursor.execute('CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(80), email VARCHAR(150))')
+    conexao.commit()
 
 
-data = ('Giovanna Silva', 'joao.silva@example.com')
-cursor.execute('INSERT INTO clientes (nome, email) VALUES (?, ?)', data)
-conexao.commit()
+def inserir_registro(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute('INSERT INTO clientes (nome, email) VALUES (?, ?)', data)
+    conexao.commit()
+
+
+def atualizar_registro(conexao, cursor, id, nome, email):
+    data = (nome, email, id)
+    cursor.execute('UPDATE clientes SET nome = ?, email = ? WHERE id=? ;', data)
+    conexao.commit()
+
+atualizar_registro(conexao, cursor, 1, 'Maria', 'maria@example.com')
+atualizar_registro(conexao, cursor, 2, 'Maria', 'giovanaAtualizada@example.com')
