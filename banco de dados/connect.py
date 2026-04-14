f02428962df6c5e15be725edb4c2d7ca924a1a41
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-14 14:17:56 -0300

    feat: adicionando registros, entendendo como se defender de SQL injection

--- banco de dados/connect.py
+++ banco de dados/connect.py
@@ -8,4 +8,9 @@ conexao = sqlite3.connect(ROOT_PATH / 'meu_banco.sqlite')
 print(conexao)
 cursor = conexao.cursor()
 
-cursor.execute('CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(80), email VARCHAR(150))')
+# cursor.execute('CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(80), email VARCHAR(150))')
+
+
+data = ('Giovanna Silva', 'joao.silva@example.com')
+cursor.execute('INSERT INTO clientes (nome, email) VALUES (?, ?)', data)
+conexao.commit()
