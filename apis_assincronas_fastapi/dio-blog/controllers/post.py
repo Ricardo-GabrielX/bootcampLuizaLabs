9e6ccadd1254ea6037ea28213450094056f53b19
Gabriel <gabrieloliveira280807@gmail.com>
2026-04-15 19:15:48 -0300

    feat: criando PostService com base no do modelo de négocio do professor, termiandno CRUD, agora temos todas as operações(get, put, post, delete)

--- apis_assincronas_fastapi/dio-blog/controllers/post.py
+++ apis_assincronas_fastapi/dio-blog/controllers/post.py
@@ -1,35 +1,39 @@
 from fastapi import APIRouter, status
-from sqlalchemy import values
-from schemas.post import PostIn
-from models.post import posts
+from schemas.post import PostIn, PostUpdateIn
 from views.post import PostOut
-from database import database
+from services.post import PostService
 
 router = APIRouter(prefix="/posts")
+service = PostService()
 
+
+# LISTAR TODOS 
 @router.get("/", response_model=list[PostOut])
-async def read_posts(published: bool, limit: int, skip: int = 0):
-    query = posts.select()
-    return await database.fetch_all(query)
+async def read_posts(published: bool = True, limit: int = 10, skip: int = 0):
+    return await service.read_all(published, limit, skip)
 
 
+# CRIAR POST
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
 async def create_post(post: PostIn):
-    command = posts.insert().values(title=post.title, content=post.content, published=post.published, published_at=post.published_at)
-    last_id = await database.execute(command)
-    return {**post.model_dump(), "id" : last_id}
+    last_id = await service.create(post)
+    return {**post.model_dump(), "id": last_id}
 
 
+# LER POST POR ID
+@router.get("/{id}", response_model=PostOut)
+async def read_post(id: int):
+    return await service.read(id)
 
 
-
-            
-        
-# Argumentos obrigátorios devem vir antes dos opcionais, caso contrário, o FastAPI não conseguirá identificar os parâmetros corretamente e retornará um erro.
-# posts?published=on -> posts?published=true
-# posts?published=off -> posts?published=false
+# ATUALIZAR POST
+@router.put("/{id}", response_model=PostOut)
+async def update_post(id: int, post: PostUpdateIn):
+    return await service.update(id, post)
 
 
-
-
-
+# DELETAR POST  
+@router.delete("/{id}", status_code=status.HTTP_200_OK)
+async def delete_post(id: int):
+    await service.delete(id)
+    return {"mensagem": f"Post de id {id} deletado com sucesso!"}
