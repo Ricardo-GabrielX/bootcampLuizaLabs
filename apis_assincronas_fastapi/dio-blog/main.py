7a173830c3612702bbb1c5eb1504908c413f48ff
Gabriel <gabrieloliveira280807@gmail.com>
2026-05-26 23:15:47 -0300

    feat: adicionando auth controller

--- apis_assincronas_fastapi/dio-blog/main.py
+++ apis_assincronas_fastapi/dio-blog/main.py
@@ -1,19 +1,21 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from controllers import post
+from controllers import auth, post
 from database import database, metadata, engine
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    from models import post
+    from models.post import posts
+    
     await database.connect()
     metadata.create_all(engine)
     yield
     await database.disconnect()
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(auth.router)
 app.include_router(post.router)
 
 
