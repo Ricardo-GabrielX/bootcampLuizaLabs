24fe56dd4c0beb16a3e970753d8bc205f32348b6
Gabriel <gabrieloliveira280807@gmail.com>
2026-05-28 14:32:59 -0300

    feat: Adicionando implementação de conftest e estrtura para testes

---
+++ apis_assincronas_fastapi/dio-blog/tests/conftest.py
@@ -0,0 +1,49 @@
+import asyncio
+from importlib.metadata import metadata
+import os
+
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+
+os.environ.setdefault("DATABASE_URL", f"sqlite:///test_blog.sqlite")
+
+
+@pytest_asyncio.fixture
+async def db(request):
+    from src.database import Database, engine, metadata
+    from src.models.post import posts
+
+    await database.connect()
+    metadata.create_all(engine)
+
+    def teardown():
+        async def _teardown():
+            await database.disconnect()
+            metadata.drop_all(engine)
+                
+        asyncio.run(_teardown())    
+
+    request.addfinalizer(teardown)
+
+
+
+@pytest_asyncio.fixture
+async def client(db):
+    from src.main import app
+
+    transport = ASGITransport(app=app)
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+    
+    async with AsyncClient(base_url="http://test", transport=transport, headers=headers) as client:
+        yield client
+
+
+
+@pytest_asyncio.fixture
+async def access_token(client: AsyncClient):
+    response = await client.post("/auth/login", json={"user_id": 1})
+    token = response.json()["access_token"]
+    return token   
